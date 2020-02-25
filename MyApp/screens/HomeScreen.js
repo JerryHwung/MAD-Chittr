bb20a03aedfb3ba2ad424ba6e7a22b0a831410ed
JerryHwung <44294098+JerryHwung@users.noreply.github.com>
2020-02-25 23:05:22 +0000

    included server files and implemented GET/chits endpoint in Homescreen
    Downloaded and extracted the server folder (v0.0.5) in the same directory as MyApp.
    Config.js has also amended to connect mudfoot, the server files are also installed into the directory. with npm install, npm test are executed to test server and the results are same as expected.
    To test the APIs, I have used POSTMAN to try out some APIs, including Login, Logout , get chits and post chits. Authorisation are tested too.
    After testing the APIs, I implemented the GET/chits endpoint into Homescreen.js. Only chit_context are shown now, other details will be shown and each chit will be clickable in a list in the future.

--- MyApp/screens/HomeScreen.js
+++ MyApp/screens/HomeScreen.js
@@ -1,15 +1,52 @@
 import React, {Component} from 'react';
-import {Text, View, Button} from 'react-native';
+import {FlatList, ActivityIndicator, Text, View} from 'react-native';
 
+// This is the home screen
 class HomeScreen extends Component{
+	// Constructor to set the states
+	constructor(props){
+		super(props);
+		this.state={
+			isLodaing: true,
+			chitListData: []
+		}
+	}
+	// A function to retrieve chits
+	getData(){
+		// Will be changed to 10.0.2.2 in the future for uni 
+		return fetch('http://192.168.0.22:3333/api/v0.0.5/chits')
+		.then((response)=>response.json())
+		.then((responseJson)=>{
+			this.setState({
+				isLoading: false,
+				chitListData: responseJson,
+			});
+		})
+		.catch((error)=>{
+			console.log(error);
+		});
+	}
+	
+	componentDidMount(){
+		this.getData();
+	}
 	
 	render(){
+		// Display activity indicator while waiting data to appear
+		if(this.state.isLoading){
+			return(
+				<View>
+				<ActivityIndicator/>
+				</View>
+			)
+		}
+		
 		return(
 			<View>
-				<Text>Home Screen</Text>
-				<Button
-					title="Chit" 
-					onPress={() => this.props.navigation.navigate('Chit')}
+				<FlatList
+					data={this.state.chitListData}
+					renderItem={({item}) => <Text>{item.chit_content}</Text>}
+					keyExtractor={({chit_id}, index) => chit_id}
 				/>
 			</View>
 		);
