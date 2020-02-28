9119b0378f885a9b51ab0f52c5c1fef2fe508ba6
JerryHwung <44294098+JerryHwung@users.noreply.github.com>
2020-02-28 22:32:51 +0000

    Implemented login and logout into app
    Implemented login endpoint into Login.js and logout endpoint in profile.js (might change in the future). Also added AsyncStorage for storing API key for future authorization.
    Redirects for logout and login need to be improve and several parts need refactor in the future.  Currently AsyncStorage only stores token provided after logged in, will add user's ID into it for authentication.

--- MyApp/screens/Login.js
+++ MyApp/screens/Login.js
@@ -1,20 +1,133 @@
-import React, {Component} from 'react';
-import {Text, View, Button} from 'react-native';
+import React, {Component, Fragment} from 'react';
+import {StyleSheet, SafeAreaView, View, Text, Alert, AsyncStorage} from 'react-native';
+import {Button, Icon} from 'react-native-elements';
+import { Formik } from 'formik';
+import {NavigationActions} from 'react-navigation';
+import FormInput from '../components/FormInput'
+import FormButton from '../components/FormButton'
+import * as yup from 'yup'
+import ErrorMessage from '../components/ErrorMessage'
 
-class Login extends Component{
+export default class Login extends Component{
+	
+	handleSubmit = values => {
+		
+		if(values.email.length>0 && values.password.length>0){
+			// 192.168.0.22
+			return fetch ('http://192.168.1.103:3333/api/v0.0.5/login',
+			{
+				method: 'POST',
+				headers: {
+					Accept: 'applicaation/json',
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({
+					email: values.email,
+					password: values.password
+				})
+			})
+			.then((response) => {
+				if(response.status == "200"){
+					this.props.navigation.navigate('Home')
+					// reponse.json() here requires return
+					// because it is within {}
+					return response.json()
+				} else {
+					Alert.alert("Wrong email/password!")
+					// return empty object to handle warning
+					return {}
+				}
+			})
+			.then(responseJson => {
+				// extract token for authorisation
+				console.log(responseJson.token);
+				// Store it with AsyncStorage
+				AsyncStorage.setItem('key', responseJson.token)
+			})
+			.catch((error)=>{
+				console.error(error);
+			})
+		}
+	}
+	
+	goToSignup = () => this.props.navigation.navigate('SignUp')
 	
 	render(){
 		return(
-			<View>
-				<Text>Login Screen</Text>
-				<Button
-					title="Sign Up" 
-					onPress={() => this.props.navigation.navigate('SignUp')}
-				/>
-			</View>
-			
-		);
+			<SafeAreaView style={styles.container}>
+				<Formik
+					initialValues={{email: '', password: ''}}
+					onSubmit={(values, {resetForm} )=> {this.handleSubmit(values)}}
+					validationSchema={validationSchema}
+				>
+					{({handleChange, values, handleSubmit, errors, isValid, isSubmitting, touched, handleBlur}) => (
+						<Fragment>
+							<ErrorMessage errorValue={touched.family_name && errors.family_name} />
+							<FormInput
+								name="Email"
+								value={values.email}
+								placeholder="Enter Email"
+								autoCapitalize="none"
+								onChangeText={handleChange('email')}
+								iconName="email"
+								iconColor="#2C384A"
+								onBlur={handleBlur('email')}
+							/>
+							<ErrorMessage errorValue={touched.email && errors.email} />
+							<FormInput
+								name="Password"
+								value={values.password}
+								placeholder="Enter Password"
+								secureTextEntry
+								onChangeText={handleChange('password')}
+								iconName="lock"
+								iconColor="#2C384A"
+								onBlur={handleBlur('password')}
+							/>
+							<ErrorMessage errorValue={touched.password && errors.password} />
+							
+							<View style={styles.buttonContainer}>
+								<FormButton
+									buttonType="outline"
+									onPress={handleSubmit}
+									title="LOGIN"
+									buttonColor="#039BE5"
+									disabled={!isValid}
+								/>
+							</View>
+							<Button
+								title="Not a member? Join Chittr today!"
+								onPress={this.goToSignup}
+								titleStyle={{
+									color: '#F57c00'
+								}}
+								type="clear"
+							/>
+						</Fragment>
+					)}
+				</Formik>	
+			</SafeAreaView>	
+		)
 	}
 }
 
-export default Login
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#fff'
+	},
+	buttonContainer: {
+		margin: 25
+	}
+})
+
+const validationSchema = yup.object().shape({
+	email: yup.string()
+		.label('Email')
+		.email('Enter a valid email')
+		.required('Please enter a registered email'),
+	password: yup.string()
+		.label('Password')
+		.required()
+		.min(4, 'Password must have at least 4 characters ')
+})
--- MyApp/screens/Profile.js
+++ MyApp/screens/Profile.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Text, View, Button} from 'react-native';
+import {Text, View, Button, AsyncStorage, Alert} from 'react-native';
 
 class Profile extends Component{
 	
@@ -11,9 +11,36 @@ class Profile extends Component{
 					title="Edit" 
 					onPress={() => this.props.navigation.navigate('EditProfile')}
 				/>
+				<Button
+					title="Logout" 
+					onPress={this.Logout}
+				/>
 			</View>
 		);
 	}
+	
+	Logout = () =>{
+		AsyncStorage.getItem('key', (err,result)=>{
+			console.log(result);
+			
+			return fetch('http://192.168.1.103:3333/api/v0.0.5/logout',{
+				method: 'POST',
+				withCredentials: true,
+				headers: {
+					'X-Authorization': result,
+					'Content-Type': 'application/json'
+				}	
+			})
+			.then((response) => {
+				AsyncStorage.removeItem('key');
+				this.props.navigation.navigate('Home')
+			})
+			.catch(function(error){
+				console.log(error);
+			})
+		})
+	}
+	
 }
 
 export default Profile
