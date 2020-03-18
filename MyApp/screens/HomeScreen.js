782826dc0a0328219d6e4b6b94300886d0f36ab3
JerryHwung <44294098+JerryHwung@users.noreply.github.com>
2020-03-18 18:32:35 +0000

    Implemented show image and pull to refresh in homescreen.js
    Added a for loop to loop through chit list and extract photo/image.
    Wrapped the file reader with a promise wrapper to make it asynchronous.
    Added pull to refresh feature in home screen to refresh page because componentDidMount only triggered once.
    Side not:e: calling setState in render() will cause infinite loop
    Will update the cards to show who post, when does it post and tagged location (if there's any).

--- MyApp/screens/HomeScreen.js
+++ MyApp/screens/HomeScreen.js
@@ -4,8 +4,6 @@ import {NavigationEvents} from 'react-navigation';
 import {ListItem, Image, Card} from 'react-native-elements';
 import {baseUrl} from '../components/baseUrl';
 
-const profilePic = require('../images/default.jpg');
-
 // This is the home screen which contains a chit list
 class HomeScreen extends Component{
 	// Constructor to set the states
@@ -13,8 +11,9 @@ class HomeScreen extends Component{
 		super(props);
 		this.state={
 			isLodaing: true,
-			photo: [],
-			chitListData: []
+			photoList: [],
+			chitListData: [],
+			refreshing: false,
 		}
 	}
 	// An async function to do GET/chits request
@@ -22,6 +21,7 @@ class HomeScreen extends Component{
 		return fetch(baseUrl+'/chits')
 		.then((response)=> response.json())
 		.then((responseJson)=>{
+			
 			this.setState({
 				chitListData: responseJson,
 			});
@@ -33,28 +33,91 @@ class HomeScreen extends Component{
 	
 	componentDidMount(){
 		this.getData()
+		.then(async()=>{
+			// Async function to retrieve photo
+			const list = [];
+			for(var chit of this.state.chitListData){
+				try{
+					let data = await this.getPhoto(chit.chit_id)
+					// made a async function to read blob data
+					let img = await this.readFileAsync(data)
+					list.push({id: chit.chit_id, image: img});
+				} catch(err){
+					console.log(err);
+				}
+			}
+			return list;
+		})
+		.then((list)=>{
+			this.setState({
+				isLoading: false,
+				photoList: list,
+				refreshing: false,
+			});
+		})
+		.catch(err=>{console.log(err)})
 	}
 	
-	/* getPhoto(chit_id){
-		fetch(baseUrl+'/chits/'+chit_id+'/photo')
-		.then((response)=> {
-			if(response.status=='200'){
-				return response.blob();
-			}else{
-				this.state.photo.push({id: chit_id, source:''})
+	// This will be called after user refresh the flatlist 
+	getApiData(){
+		this.getData()
+		.then(async()=>{
+			const list = [];
+			for(var chit of this.state.chitListData){
+				try{
+					let data = await this.getPhoto(chit.chit_id)
+					let img = await this.readFileAsync(data)
+					list.push({id: chit.chit_id, image: img});
+				} catch(err){
+					console.log(err);
+				}
 			}
+			return list;
 		})
-		.then((image)=>{
-			var reader = new FileReader();
-			reader.onload =()=>{
-				this.state.photo.push({id: chit_id, source: reader.result})
-			}
-			reader.readAsDataURL(image);
+		.then((list)=>{
+			this.setState({
+				isLoading: false,
+				photoList: list,
+				refreshing: false,
+			});
 		})
-		.catch((err)=>{
-			console.log(err);
-		});
-	} */
+		.catch(err=>{console.log(err)})
+	}
+	
+	// Used promise wrapper for file reading
+	readFileAsync(file){
+		return new Promise((resolve, reject)=>{
+			let reader = new FileReader();
+			reader.onload = () => {
+				resolve(reader.result);
+			};
+			reader.onerror = reject;
+			reader.readAsDataURL(file);
+		})
+	}
+	// Extract photo as a blob data
+	async getPhoto(chit_id){
+		let response = await fetch(baseUrl+'/chits/'+chit_id+'/photo')
+		if(response.status=='200'){
+			return await response.blob();
+		} else {
+			return null;
+		}
+		
+	}
+	
+	handleRefresh = () => {
+		this.setState({
+			isFetching: true
+		}, function(){this.getApiData()});
+	}
+	// Get image uri from state and return an image
+	showImage(chit_id){
+		let response = this.state.photoList.find(img => img.id == chit_id);
+		if(response){
+		return (<Image source={{uri: response.image}} style={{height: 200, width: 200}}/>)
+		}
+	}
 	
 	render(){
 		// Display activity indicator while waiting data to appear
@@ -64,33 +127,29 @@ class HomeScreen extends Component{
 				<ActivityIndicator/>
 				</View>
 			)
-		}
+		}else{
 		// iterate data in a flatlist
-		return(
-			<View>
-			<NavigationEvents onDidFocus={() => this.getData()}/>
-				<FlatList
-					data={this.state.chitListData}
-					renderItem={({item}) => (
-						/* <ListItem 
-							title={item.chit_content}
-							subtitle={new Date(item.timestamp).toUTCString()}
-							leftAvatar={{source: profilePic}}
-							bottomDivider
-							chevron
-							onPress={() => console.log("check chit")}
-						/> */
-						<Card>
-							<View>
-								<Text>{item.chit_content}</Text>
-							</View>
-						</Card>
-						
-					)}
-					keyExtractor={({chit_id}, index) => chit_id.toString()}
-				/>
-			</View>
-		);
+		// This flatlist implemnted a pull to refresh function
+			return(
+				<View>
+					<FlatList
+						data={this.state.chitListData}
+						renderItem={({item}) => (
+							<Card>
+								<View>
+									<Text>{item.chit_content}</Text>
+									{this.showImage(item.chit_id)}
+								</View>
+							</Card>
+							
+						)}
+						keyExtractor={({chit_id}, index) => chit_id.toString()}
+						refreshing={this.state.refreshing}
+						onRefresh={this.handleRefresh}
+					/>
+				</View>
+			);
+		}
 	}
 }
 
